650aeff02998fd66154da32dc3ceed765d52d3d9
Tomasz Galecki <saleph@protonmail.com>
2019-12-05 09:17:03 +0100

    Extract to function

--- adjacency_list_builder.py
+++ adjacency_list_builder.py
@@ -8,74 +8,77 @@ first_phase_json_cache_links = '1st_phase_links.json'
 first_phase_json_cache_title_cache = '1st_phase_title_cache.json'
 second_phase_json_cache_links = '2nd_phase_links_FULL_SOLUTION.json'
 
-links = defaultdict(dict) # links[namespace_id][page_id] = {namespace_id:[1,4,5]}
-title_cache = defaultdict(dict)
+def generate_pages_links():
+    links = defaultdict(dict) # links[namespace_id][page_id] = {namespace_id:[1,4,5]}
+    title_cache = defaultdict(dict)
 
-if os.path.isfile(second_phase_json_cache_links):
-    print("Full solution cache available. Reading the pages with links, will take a long time...")
-    with open(second_phase_json_cache_links, 'r') as f:
-        links = json.load(f)
-else:
-    if os.path.isfile(first_phase_json_cache_links) and os.path.isfile(first_phase_json_cache_title_cache):
-        print("Cache available. Reading the pages without links...")
-        with open(first_phase_json_cache_links, 'r') as f:
+    if os.path.isfile(second_phase_json_cache_links):
+        print("Full solution cache available. Reading the pages with links, will take a long time...")
+        with open(second_phase_json_cache_links, 'r') as f:
             links = json.load(f)
-        print("Reading title to id lookup table...")
-        with open(first_phase_json_cache_title_cache, 'r') as f:
-            title_cache = json.load(f)
-        print("Cache successfully restored")
     else:
-        print("No cache available")
-        pages_pattern = re.compile("\\((\d+),(.+?),'(.*?)',.*?,NULL\\)")
-        with open("C:/Users/togal1/Downloads/data/plwiki-latest-page.sql", "r", encoding="utf8") as file:
-            print("Start of the pages processing (around 3100k)")
+        if os.path.isfile(first_phase_json_cache_links) and os.path.isfile(first_phase_json_cache_title_cache):
+            print("Cache available. Reading the pages without links...")
+            with open(first_phase_json_cache_links, 'r') as f:
+                links = json.load(f)
+            print("Reading title to id lookup table...")
+            with open(first_phase_json_cache_title_cache, 'r') as f:
+                title_cache = json.load(f)
+            print("Cache successfully restored")
+        else:
+            print("No cache available")
+            pages_pattern = re.compile("\\((\d+),(.+?),'(.*?)',.*?,NULL\\)")
+            with open("C:/Users/togal1/Downloads/data/plwiki-latest-page.sql", "r", encoding="utf8") as file:
+                print("Start of the pages processing (around 3100k)")
+                counter = 0
+                for line in file:
+                    for match in re.finditer(pages_pattern, line):
+                        page_id = int(match.group(1))
+                        namespace_id = int(match.group(2))
+                        title = match.group(3)
+                        links[namespace_id][page_id] = defaultdict(list)
+                        title_cache[namespace_id][title] = page_id
+
+                        counter += 1
+                        if counter % 100000 == 0:
+                            print("Pages processed: " + str(counter//1000) + "k")
+
+            print("Pages processed. Caching pages...")
+            with open(first_phase_json_cache_links, 'w') as fp:
+                json.dump(links, fp)
+            print("Pages cached. Caching title to id lookup table...")
+            with open(first_phase_json_cache_title_cache, 'w') as fp:
+                json.dump(title_cache, fp)
+            print("All the caching done")
+
+        pattern_links = re.compile("\\((\d+),(\d+),'(.*?)',(\d+)\\)[,;]")
+        with open("C:/Users/togal1/Downloads/data/plwiki-latest-pagelinks.sql", "r", encoding="utf8") as file:
+            print("Start of the links processing (around 150kk)")
             counter = 0
             for line in file:
-                for match in re.finditer(pages_pattern, line):
-                    page_id = int(match.group(1))
-                    namespace_id = int(match.group(2))
-                    title = match.group(3)
-                    links[namespace_id][page_id] = defaultdict(list)
-                    title_cache[namespace_id][title] = page_id
+                for match in re.finditer(pattern_links, line):
+                    from_id = int(match.group(1))
+                    from_namespace = int(match.group(4))
+                    to_namespace = int(match.group(2))
+                    to_title = match.group(3)
+
+                    to_id = 0
+                    try:
+                        to_id = title_cache[to_namespace][to_title]
+                        links[from_namespace][from_id][to_namespace].append(to_id)
+                    except:
+                        # links may be invalid, as stated: https://www.mediawiki.org/wiki/Manual:Pagelinks_table
+                        continue
 
                     counter += 1
-                    if counter % 100000 == 0:
-                        print("Pages processed: " + str(counter//1000) + "k")
+                    if counter % 1000000 == 0:
+                        print("Links processed: " + str(counter//1000000) + "kk")
 
-        print("Pages processed. Caching pages...")
-        with open(first_phase_json_cache_links, 'w') as fp:
+        print("Links processed. Caching pages with links...")
+        with open(second_phase_json_cache_links, 'w') as fp:
             json.dump(links, fp)
-        print("Pages cached. Caching title to id lookup table...")
-        with open(first_phase_json_cache_title_cache, 'w') as fp:
-            json.dump(title_cache, fp)
-        print("All the caching done")
+        print("Caching done")
+    return links
 
-    pattern_links = re.compile("\\((\d+),(\d+),'(.*?)',(\d+)\\)[,;]")
-    with open("C:/Users/togal1/Downloads/data/plwiki-latest-pagelinks.sql", "r", encoding="utf8") as file:
-        print("Start of the links processing (around 150kk)")
-        counter = 0
-        for line in file:
-            for match in re.finditer(pattern_links, line):
-                from_id = int(match.group(1))
-                from_namespace = int(match.group(4))
-                to_namespace = int(match.group(2))
-                to_title = match.group(3)
-
-                to_id = 0
-                try:
-                    to_id = title_cache[to_namespace][to_title]
-                    links[from_namespace][from_id][to_namespace].append(to_id)
-                except:
-                    # links may be invalid, as stated: https://www.mediawiki.org/wiki/Manual:Pagelinks_table
-                    continue
-
-                counter += 1
-                if counter % 1000000 == 0:
-                    print("Links processed: " + str(counter//1000000) + "kk")
-
-    print("Links processed. Caching pages with links...")
-    with open(second_phase_json_cache_links, 'w') as fp:
-        json.dump(links, fp)
-    print("Caching done")
-
+links = generate_pages_links()
 print("Number of pages with links stored: " + str(len(links)))
